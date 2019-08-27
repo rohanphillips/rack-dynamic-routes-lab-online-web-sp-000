1dc482e6a4b6ea70ff256a9c33b2af032dc64fb8
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-27 16:28:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       if item != nil
         resp.write(item.price)
       else
-        resp.write("Route not found")
+        resp.write("Item not found")
         resp.status = 404
       end
     else
