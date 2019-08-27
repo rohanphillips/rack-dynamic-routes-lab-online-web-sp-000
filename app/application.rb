18669ffb16c39a1a563a1b6f9b6ddd5dc78208f8
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-27 16:15:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       add_item = req.params["item"]
       item = @@items.find{|item| item.name == add_item}
-      resp.write(item)
+      resp.write(add_item)
     else
       resp.write "Route not found"
       resp.status = 404
